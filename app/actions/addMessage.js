2b6f1c6085a5706d18fb072af537cfe5934f152e
Biswajeet Sanyal <c-biswajeet.sanyal@timesofindia.com>
2026-02-09 15:55:13 +0530

    Fix addMessage

--- app/actions/addMessage.js
+++ app/actions/addMessage.js
@@ -8,35 +8,43 @@ import { get_server_session } from "@/util/serverSession";
 
 
 async function addMessage(previousState, formData) {
-  await connectDB();
 
-  const sessionUser = await get_server_session();
+  try {
+    await connectDB();
 
-  if (!sessionUser || !sessionUser.userId) {
-    return { error: 'You must be logged in to send a message' };
+    const sessionUser = await get_server_session();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return { error: 'You must be logged in to send a message' };
+    }
+
+    const { userId } = sessionUser;
+
+    const { recipient, property, name, email, phone, body } = formData;
+
+    if (userId === recipient) {
+      return { error: 'You can not send a message to yourself' };
+    }
+
+    const newMessage = new Message({
+      sender: userId,
+      recipient,
+      property,
+      name,
+      email,
+      phone,
+      body,
+    });
+
+    await newMessage.save();
+
+    return { submitted: true };
+
+  } catch (error) {
+    console.log("Error in sending message ", error);
+    return;
   }
 
-  const { userId } = sessionUser;
-
-  const { recipient, property, name, email, phone, body } = formData;
-
-  if (userId === recipient) {
-    return { error: 'You can not send a message to yourself' };
-  }
-
-  const newMessage = new Message({
-    sender: userId,
-    recipient,
-    property,
-    name,
-    email,
-    phone,
-    body,
-  });
-
-  await newMessage.save();
-
-  return { submitted: true };
 }
 
 export default addMessage;
